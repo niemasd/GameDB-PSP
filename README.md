9ce4503cb9ca642cdfa9296a3be2429fa000234e
Niema Moshiri <niemamoshiri@gmail.com>
2023-10-27 08:07:52 -0700

    Added helper script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GameDB-PSP
 Sony PlayStation Portable (PSP), part of [GameDB](https://github.com/niemasd/GameDB).
+
+# Sources
+* [PlayStation Datacenter](https://psxdatacenter.com/)
+* [Redump](http://redump.org/)
---
+++ helper/psxdatacenter_dump/scrape_psxdatacenter_main.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python3
+'''
+Scrape titles + serials from PlayStation DataCenter main pages
+'''
+from bs4 import BeautifulSoup
+from datetime import datetime
+from os import makedirs
+from os.path import abspath, expanduser, isfile
+from urllib.request import urlopen
+from sys import argv
+
+# PlayStation DataCenter PSP main pages
+URLS = {
+    'NTSC-U': 'https://psxdatacenter.com/psp/ulist.html',
+    'NTSC-J': 'https://psxdatacenter.com/psp/jlist.html',
+    'PAL': 'https://psxdatacenter.com/psp/plist.html',
+}
+
+# language map (need to upper() the original ones first)
+LANG = {
+    'A':   'Arabic',
+    'AF':  'Afrikaans',
+    'AR':  'Arabic',
+    'C':   'Czech',
+    'CA':  'Catalan',
+    'CAT': 'Catalan',
+    'CH':  'Chinese',
+    'CJ':  'Cantonese', # not sure
+    'CR':  'Croatian', # not sure
+    'CZ':  'Czech',
+    'D':   'Danish',
+    'DU':  'Dutch',
+    'E':   'English',
+    'F':   'French',
+    'FI':  'Finnish',
+    'FL':  'Finnish',
+    'G':   'German',
+    'GA':  'Gaelic',
+    'GR':  'Greek',
+    'H':   'Hungarian',
+    'HI':  'Hindi',
+    'HU':  'Hungarian',
+    'I':   'Italian',
+    'J':   'Japanese',
+    'K':   'Korean',
+    'N':   'Norwegian',
+    'NW':  'Norwegian',
+    'P':   'Portuguese',
+    'PL':  'Polish',
+    'PO':  'Polish', # not sure
+    'R':   'Russian',
+    'S':   'Spanish',
+    'SC':  'Scandinavian',
+    'SW':  'Swedish',
+    'T':   'Turkish',
+}
+
+# clean a string
+def clean(s):
+    return s.replace(chr(65533),'').replace(chr(0),'').replace(u'\xa0 ',u' ').replace(u'\xa0',u' ').strip()
+
+# main program
+if __name__ == "__main__":
+    games_path = '%s/games' % '/'.join(abspath(expanduser(argv[0])).split('/')[:-3])
+    for region, url in URLS.items():
+        print("Parsing %s..." % region)
+        raw_data = clean(urlopen(url).read().decode('utf-8', errors='replace'))
+        raw_data = raw_data.replace('<tr>\n<tr>','<tr>')
+        raw_data = raw_data.replace('</td>\n</td>', '</td>\n</tr>')
+        raw_data = raw_data.replace('<td>\n</tr>', '</td>\n</tr>')
+        raw_data = raw_data.replace('</td>\n<tr>', '</td>\n</tr>\n<tr>')
+        soup = BeautifulSoup(raw_data, 'html.parser')
+        for row in soup.find_all('tr'):
+            cols = [clean(v.text) for v in row.find_all('td')]
+            if len(cols) == 0:
+                continue
+            DUMMY, serial, title, language_s = cols
+            language = list()
+            for lang in language_s.upper().replace('[[','][').replace('<','').replace('/TD>','').replace(';','').replace('`','').replace('J[E]','[J][E]').replace('(N(SW)','(N)(SW)').replace('{','').replace('(','').replace('}',']').replace(')',']').replace('[','').split(']'):
+                if len(lang) != 0:
+                    if lang not in LANG:
+                        print("MISSING: %s\n%s" % (lang, cols)); exit(1)
+                    language.append(LANG[lang])
+            game_path = '%s/%s' % (games_path, serial); makedirs(game_path, exist_ok=True)
+            title_path = '%s/title.txt' % game_path
+            if not isfile(title_path):
+                f = open(title_path, 'w'); f.write('%s\n' % title); f.close()
+            language_path = '%s/language.txt' % game_path
+            if len(language) != 0 and not isfile(language_path):
+                f = open(language_path, 'w'); f.write('%s\n' % '\n'.join(language)); f.close()
+            region_path = '%s/region.txt' % game_path
+            f = open(region_path, 'w'); f.write('%s\n' % region); f.close()
